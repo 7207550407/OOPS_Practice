ee6e940ef7691617979b8768dfe43f735db1f566
7207550407 <83601639+7207550407@users.noreply.github.com>
2021-05-19 02:23:13 -0400

    Add files via upload
    nenu ivala static instance pracice chesanu..ee files lo upload chesanu

---
+++ Addition.java
@@ -0,0 +1,16 @@
+class Addition
+{
+	static String name="Sai";
+	int number=10;
+	
+	public static int additionOfTwoNumbers(int a, int b)
+	{
+			return a+b;
+	}
+	
+	public int additionOfThreeNumbers(int a, int b, int c)
+	{
+		return a+b+c;
+	}
+	
+}
---
+++ Practice1.java
@@ -0,0 +1,21 @@
+class Practice1
+
+{
+	
+	
+	
+	public static void main(String[]xyz)
+	{
+		
+		
+			/*String s= Addition.name;
+			System.out.println(s);
+			Addition a=new Addition();
+			int b=a.number;                
+			System.out.println(b);*/
+	}	
+	
+	
+	
+	
+}
