bc8a3368c60b4dda7c18f706ece3ba68e1ef78db
7207550407 <83601639+7207550407@users.noreply.github.com>
2021-05-25 09:41:31 -0400

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,21 @@
+package practice;
+
+public class Rectangle 
+{
+	int length;
+	int breadth;
+	public Rectangle(int l, int b)
+	{
+		length = l;
+		breadth = b;
+		
+	}
+	public void print()
+	{
+		System.out.println(length*breadth);
+	}
+	public void prints()
+	{
+		System.out.println(length+breadth);
+	}
+}
